f2043317db540048cf1c061ec5ea88124a9e21e4
Oscar Santos <oscar303data@gmail.com>
2023-09-11 14:20:07 -0600

    create frontend and backend folders of the App

---
+++ backend/server.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+
+const port = 4000;
+
+
+app.use(cors());
+app.use(bodyParser.json());
+
+
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+})
+
+
+
+
+
+
+app.listen(port, () => {
+    console.log('Example app listening on port', port);
+});
